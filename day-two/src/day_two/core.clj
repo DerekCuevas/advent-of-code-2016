f9c2c2b2d9afd6732a470c653f150386c548f1e2
Derek Cuevas <dccuevas2@gmail.com>
2017-01-30 08:25:36 -0800

    added private functions

--- day-two/src/day_two/core.clj
+++ day-two/src/day_two/core.clj
@@ -1,23 +1,23 @@
 (ns day-two.core
   (:gen-class))
 
-(defn move [direction [row column]]
+(defn- move [direction [row column]]
   (case direction
     \U [(dec row) column]
     \R [row (inc column)]
     \D [(inc row) column]
     \L [row (dec column)]))
 
-(defn move-inbounds [lock position direction]
+(defn- move-inbounds [lock position direction]
   (let [next (move direction position)]
     (if (get-in lock next)
       next
       position)))
 
-(defn travel [lock position line]
+(defn- travel [lock position line]
   (reduce (partial move-inbounds lock) position line))
 
-(defn decode [lock {:keys [position code]} line]
+(defn- decode [lock {:keys [position code]} line]
   (let [next (travel lock position line)]
     {:position next
      :code (str code (get-in lock next))}))

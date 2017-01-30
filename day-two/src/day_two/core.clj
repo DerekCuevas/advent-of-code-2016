24dfd52c1aaf3cae8d45761cad5879a5e2519eff
Derek Cuevas <dccuevas2@gmail.com>
2017-01-29 17:19:17 -0800

    added first solution to day-two parts one and two

--- day-two/src/day_two/core.clj
+++ day-two/src/day_two/core.clj
@@ -1,7 +1,32 @@
 (ns day-two.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn move [direction [row column]]
+  (case direction
+    \U [(dec row) column]
+    \R [row (inc column)]
+    \D [(inc row) column]
+    \L [row (dec column)]))
+
+(defn inbounds? [lock position]
+  (not (nil? (get-in lock position))))
+
+(defn move-inbounds [lock position direction]
+  (let [next (move direction position)]
+    (if (inbounds? lock next)
+      next
+      position)))
+
+(defn travel [lock position instructions]
+  (reduce (partial move-inbounds lock) position instructions))
+
+(defn unlock [lock initial-position input]
+  (loop [position initial-position
+         [instructions & instructions-left] input
+         code ""]
+    (if (empty? instructions)
+      code
+      (let [next-position (travel lock position instructions)]
+        (recur next-position
+               instructions-left
+               (str code (get-in lock next-position)))))))
--- day-two/test/day_two/core_test.clj
+++ day-two/test/day_two/core_test.clj
@@ -1,7 +1,26 @@
 (ns day-two.core-test
   (:require [clojure.test :refer :all]
-            [day-two.core :refer :all]))
+            [day-two.core :refer :all]
+            [clojure.string :refer [split]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def part-one-lock
+  [[1 2 3]
+   [4 5 6]
+   [7 8 9]])
+
+(def part-two-lock
+  [[nil nil 1 nil nil]
+   [nil 2 3 4 nil]
+   [5 6 7 8 9]
+   [nil "A" "B" "C" nil]
+   [nil nil "D" nil nil]])
+
+(def input
+  (-> "resources/input.txt"
+      (slurp)
+      (split #"\n")))
+
+(deftest unlock-test
+  (testing "unlock"
+    (is (= (unlock part-one-lock [1 1] input) "53255"))
+    (is (= (unlock part-two-lock [2 0] input) "7423A"))))

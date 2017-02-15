673c109a3b77ccab3ac85abc616eb3778805e94d
Derek Cuevas <dccuevas2@gmail.com>
2017-02-14 21:16:52 -0800

    added solution for day four part two

--- day-four/src/day_four/core.clj
+++ day-four/src/day_four/core.clj
@@ -6,7 +6,7 @@
 
 (defn- parse [s]
   (let [[_ encrypted-name sector-id checksum] (re-find encrypted-format s)]
-    {:encrypted-name (replace encrypted-name #"-" "")
+    {:encrypted-name encrypted-name
      :sector-id (read-string sector-id)
      :checksum checksum}))
 
@@ -16,16 +16,48 @@
     (compare v2 v1)))
 
 (defn- valid-checksum? [{:keys [encrypted-name checksum]}]
-  (->> (frequencies encrypted-name)
+  (->> (replace encrypted-name #"-" "")
+       (frequencies)
        (sort comparitor)
        (map first)
        (take 5)
        (apply str)
        (= checksum)))
 
-(defn sum-sector-ids [input]
+(defn- filter-decoy [input]
   (->> input
        (map parse)
-       (filter valid-checksum?)
+       (filter valid-checksum?)))
+
+(defn- incc [c]
+ (case c
+   \z \a
+   \space \space
+   (char (inc (int c)))))
+
+(defn- rotate [n c]
+ (reduce (fn [a _] (incc a)) c (range n)))
+
+(defn- shift [n s]
+  (->> (replace s #"-" " ")
+       (map (partial rotate n))
+       (apply str)))
+
+(defn- decrypt-name [{:keys [encrypted-name sector-id] :as data}]
+  (assoc data :name (shift sector-id encrypted-name)))
+
+;; part one
+(defn sum-sector-ids [input]
+  (->> input
+       (filter-decoy)
        (map :sector-id)
        (apply +)))
+
+;; part two
+(defn find-sector-id [input]
+  (->> input
+       (filter-decoy)
+       (map decrypt-name)
+       (filter #(= (:name %) "northpole object storage"))
+       (first)
+       (:sector-id)))
--- day-four/test/day_four/core_test.clj
+++ day-four/test/day_four/core_test.clj
@@ -10,4 +10,8 @@
 
 (deftest sum-sector-ids-test
   (testing "sum sector ids"
-    (is (= (sum-sector-ids input) 1))))
+    (is (= (sum-sector-ids input) 173787))))
+
+(deftest find-sector-id-test
+  (testing "find sector id"
+    (is (= (find-sector-id input) 548))))

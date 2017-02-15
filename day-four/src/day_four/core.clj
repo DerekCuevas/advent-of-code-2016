3d97fb92b13f273d45d6510f5843887e8bb9019f
Derek Cuevas <dccuevas2@gmail.com>
2017-02-14 19:54:32 -0800

    added parsing functions for day four

--- day-four/src/day_four/core.clj
+++ day-four/src/day_four/core.clj
@@ -1,5 +1,14 @@
 (ns day-four.core
+  (:require [clojure.string :refer [replace]])
   (:gen-class))
 
+(def format #"(\S+)-(\d+)\[(\S+)\]")
+
+(defn parse [s]
+  (let [[_ encrypted-name sector-id checksum] (re-find format s)]
+    {:encrypted-name (replace encrypted-name #"-" "")
+     :sector-id (read-string sector-id)
+     :checksum checksum}))
+
 (defn sum-sector-ids [input]
-  0)
+  (map parse input))

68ecede3d45fbb0932ac414e583ecae741f4bee6
Derek Cuevas <dccuevas2@gmail.com>
2017-02-14 20:24:26 -0800

    added solution for day four part one

--- day-four/src/day_four/core.clj
+++ day-four/src/day_four/core.clj
@@ -2,13 +2,30 @@
   (:require [clojure.string :refer [replace]])
   (:gen-class))
 
-(def format #"(\S+)-(\d+)\[(\S+)\]")
+(def ^:private encrypted-format #"(\S+)-(\d+)\[(\S+)\]")
 
-(defn parse [s]
-  (let [[_ encrypted-name sector-id checksum] (re-find format s)]
+(defn- parse [s]
+  (let [[_ encrypted-name sector-id checksum] (re-find encrypted-format s)]
     {:encrypted-name (replace encrypted-name #"-" "")
      :sector-id (read-string sector-id)
      :checksum checksum}))
 
+(defn- comparitor [[k1 v1] [k2 v2]]
+  (if (= v1 v2)
+    (compare k1 k2)
+    (compare v2 v1)))
+
+(defn- valid-checksum? [{:keys [encrypted-name checksum]}]
+  (->> (frequencies encrypted-name)
+       (sort comparitor)
+       (map first)
+       (take 5)
+       (apply str)
+       (= checksum)))
+
 (defn sum-sector-ids [input]
-  (map parse input))
+  (->> input
+       (map parse)
+       (filter valid-checksum?)
+       (map :sector-id)
+       (apply +)))

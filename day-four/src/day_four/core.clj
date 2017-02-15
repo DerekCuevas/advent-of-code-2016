1685fd0333972aabe38cc2390a2876cb96426d56
Derek Cuevas <dccuevas2@gmail.com>
2017-02-15 08:04:55 -0800

    cleaning up day four solution

--- day-four/src/day_four/core.clj
+++ day-four/src/day_four/core.clj
@@ -1,5 +1,5 @@
 (ns day-four.core
-  (:require [clojure.string :refer [replace]])
+  (:require [clojure.string :refer [split join replace]])
   (:gen-class))
 
 (def ^:private encrypted-format #"(\S+)-(\d+)\[(\S+)\]")
@@ -10,7 +10,7 @@
      :sector-id (read-string sector-id)
      :checksum checksum}))
 
-(defn- comparitor [[k1 v1] [k2 v2]]
+(defn- compare-frequencies [[k1 v1] [k2 v2]]
   (if (= v1 v2)
     (compare k1 k2)
     (compare v2 v1)))
@@ -18,7 +18,7 @@
 (defn- valid-checksum? [{:keys [encrypted-name checksum]}]
   (->> (replace encrypted-name #"-" "")
        (frequencies)
-       (sort comparitor)
+       (sort compare-frequencies)
        (map first)
        (take 5)
        (apply str)
@@ -29,22 +29,22 @@
        (map parse)
        (filter valid-checksum?)))
 
-(defn- incc [c]
- (case c
-   \z \a
-   \space \space
-   (char (inc (int c)))))
-
 (defn- rotate [n c]
- (reduce (fn [a _] (incc a)) c (range n)))
+  (-> (int c)
+      (+ n)
+      (- (int \a))
+      (mod 26)
+      (+ (int \a))
+      (char)))
 
 (defn- shift [n s]
-  (->> (replace s #"-" " ")
-       (map (partial rotate n))
-       (apply str)))
+  (apply str (map (partial rotate n) s)))
 
 (defn- decrypt-name [{:keys [encrypted-name sector-id] :as data}]
-  (assoc data :name (shift sector-id encrypted-name)))
+  (->> (split encrypted-name #"-")
+       (map (partial shift sector-id))
+       (join " ")
+       (assoc data :name)))
 
 ;; part one
 (defn sum-sector-ids [input]

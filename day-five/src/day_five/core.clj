d8477dbda0661d3c97f5dce307b090210f2dc7fd
Derek Cuevas <dccuevas2@gmail.com>
2017-03-01 08:05:48 -0800

    refactoring

--- day-five/src/day_five/core.clj
+++ day-five/src/day_five/core.clj
@@ -3,17 +3,14 @@
             [digest :refer [md5]])
   (:gen-class))
 
-(defn- starts-with-five-zeros? [s]
-  (->> (repeat 5 "0")
-       (apply str)
-       (starts-with? s)))
-
 (defn- hashes
   ([key]
     (hashes key 0))
   ([key n]
-    (lazy-seq (cons (md5 (str key n))
-              (hashes key (inc n))))))
+    (lazy-seq (cons (md5 (str key n)) (hashes key (inc n))))))
+
+(defn- starts-with-five-zeros? [s]
+  (starts-with? s "00000"))
 
 (defn part-one-password [key]
   (->> (hashes key)
@@ -22,21 +19,18 @@
        (map #(nth % 5))
        (apply str)))
 
-(defn- complete? [password]
-  (not-any? #{\_} password))
-
 (defn- assoc-str [s index value]
   (apply str (assoc (vec s) index value)))
 
 (defn- next-password [password hash]
   (let [position (read-string (str "0x" (nth hash 5)))
         value (nth hash 6)]
-    (if (and (< position 8) (= (get password position) \_))
-      (assoc-str password position value)
-      password)))
+    (if (or (>= position 8) (not= (get password position) \_))
+      password
+      (assoc-str password position value))))
 
 (defn- add [password hash]
-  (if (complete? password)
+  (if (not-any? #{\_} password)
     (reduced password)
     (next-password password hash)))
 

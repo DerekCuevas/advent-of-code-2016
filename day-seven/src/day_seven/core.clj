e4afa548ebd5fab0e29907c4a61c83223d1afce5
Derek Cuevas <dccuevas2@gmail.com>
2017-03-11 22:13:51 -0800

    day seven clean up

--- day-seven/src/day_seven/core.clj
+++ day-seven/src/day_seven/core.clj
@@ -1,35 +1,32 @@
 (ns day-seven.core
   (:gen-class))
 
-(defn- mirror? [coll]
+(defn- palindrome? [coll]
   (and (apply not= coll)
-       (= (apply str (take 2 coll))
-          (apply str (reverse (drop 2 coll))))))
+       (= (seq coll) (reverse coll))))
 
 (defn- abba? [s]
-  (->> (partition 4 1 s)
-       (some mirror?)))
+  (some palindrome? (partition 4 1 s)))
 
-(defn- any-abba? [seqs]
-  (some true? (map abba? seqs)))
+(defn- some-abba? [seqs]
+  (some identity (map abba? seqs)))
 
 (defn- supports-tsl? [ip]
-  (and (not (any-abba? (:hypernet-seqs ip)))
-       (any-abba? (:supernet-seqs ip))))
+  (and (some-abba? (:supernet-seqs ip))
+       (not (some-abba? (:hypernet-seqs ip)))))
 
 (defn count-supporting-tsl? [ips]
-  (count (filter supports-tsl? ips)))
+  (->> ips
+       (filter supports-tsl?)
+       (count)))
 
 ;; part two
 
 (defn- aba->bab [[a b _]]
   (seq [b a b]))
 
-(defn- aba? [s]
-  (and (apply not= s) (= (first s) (last s))))
-
 (defn- find-abas [seqs]
-  (mapcat #(filter aba? (partition 3 1 %)) seqs))
+  (mapcat #(filter palindrome? (partition 3 1 %)) seqs))
 
 (defn- supports-ssl? [ip]
   (let [abas (find-abas (:supernet-seqs ip))]
@@ -37,4 +34,6 @@
           (mapcat #(partition 3 1 %) (:hypernet-seqs ip)))))
 
 (defn count-supporting-ssl? [ips]
-  (count (filter supports-ssl? ips)))
+  (->> ips
+       (filter supports-ssl?)
+       (count)))

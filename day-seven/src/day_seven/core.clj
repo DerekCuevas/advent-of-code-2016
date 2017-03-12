498bd2f6402f867daf95a75a96ec3805fa314760
Derek Cuevas <dccuevas2@gmail.com>
2017-03-11 16:46:10 -0800

    added solution for day seven part one

--- day-seven/src/day_seven/core.clj
+++ day-seven/src/day_seven/core.clj
@@ -1,7 +1,21 @@
 (ns day-seven.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn- mirror? [coll]
+  (and (apply not= coll)
+       (= (apply str (take 2 coll))
+          (apply str (reverse (drop 2 coll))))))
+
+(defn- abba? [s]
+  (->> (partition 4 1 s)
+       (map mirror?)
+       (some true?)))
+
+(defn- ip? [{:keys [hypernet abba]}]
+  (and (not-any? true? (map abba? hypernet))
+       (some true? (map abba? abba))))
+
+(defn count-valid-ips [input]
+  (->> input
+       (filter ip?)
+       (count)))
--- day-seven/test/day_seven/core_test.clj
+++ day-seven/test/day_seven/core_test.clj
@@ -3,10 +3,31 @@
             [day-seven.core :refer :all]
             [clojure.string :refer [split]]))
 
+(def ip-format #"\[[^\]]*\]|(\b\w+\b)")
+
+(defn- hypernet-seq [matches]
+  (->> matches
+      (map #(if-not (second %) (first %) false))
+      (filter identity)))
+
+(defn- abba-seq [matches]
+  (->> matches
+       (map second)
+       (filter identity)))
+
+(defn- parse-ip [s]
+  (let [matches (re-seq ip-format s)]
+    {:hypernet (hypernet-seq matches)
+     :abba (abba-seq matches)}))
+
+(defn- parse [input]
+  (map parse-ip input))
+
 (def input
   (-> (slurp "resources/input.txt")
-      (split #"\n")))
+      (split #"\n")
+      (parse)))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (not= 1 1))))
+(deftest count-valid-ips-test
+  (testing "count valid ips"
+    (is (= (count-valid-ips input) 105))))

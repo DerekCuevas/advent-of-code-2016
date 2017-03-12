9f1f8fd6c0293dd101530365215651c1f50974ff
Derek Cuevas <dccuevas2@gmail.com>
2017-03-12 10:00:30 -0700

    more cleanups/refactoring day seven

--- day-seven/src/day_seven/core.clj
+++ day-seven/src/day_seven/core.clj
@@ -5,11 +5,14 @@
   (and (apply not= coll)
        (= (seq coll) (reverse coll))))
 
-(defn- abba? [s]
-  (some palindrome? (partition 4 1 s)))
+(defn- palindromic-subseqs [n coll]
+  (filter palindrome? (partition n 1 coll)))
 
-(defn- some-abba? [seqs]
-  (some identity (map abba? seqs)))
+(defn- combine-palindromic-subseqs [n colls]
+  (mapcat (partial palindromic-subseqs n) colls))
+
+(defn- some-abba? [colls]
+  (not (empty? (combine-palindromic-subseqs 4 colls))))
 
 (defn- supports-tsl? [ip]
   (and (some-abba? (:supernet-seqs ip))
@@ -25,13 +28,10 @@
 (defn- aba->bab [[a b _]]
   (seq [b a b]))
 
-(defn- find-abas [seqs]
-  (mapcat #(filter palindrome? (partition 3 1 %)) seqs))
-
 (defn- supports-ssl? [ip]
-  (let [abas (find-abas (:supernet-seqs ip))]
-    (some (into #{} (map aba->bab abas))
-          (mapcat #(partition 3 1 %) (:hypernet-seqs ip)))))
+  (let [abas (combine-palindromic-subseqs 3 (:supernet-seqs ip))
+        babs (combine-palindromic-subseqs 3 (:hypernet-seqs ip))]
+    (some (into #{} (map aba->bab abas)) babs)))
 
 (defn count-supporting-ssl? [ips]
   (->> ips

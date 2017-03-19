78f25d2a64f7a65bf756a753daa2366e19c32f4a
Derek Cuevas <dccuevas2@gmail.com>
2017-03-18 20:37:44 -0700

    added solution for day-eight part one

--- day-eight/src/day_eight/core.clj
+++ day-eight/src/day_eight/core.clj
@@ -17,18 +17,32 @@
 (defn- rect [[x y] value grid]
   (reduce #(assoc-in %1 %2 value) grid (coordinates x y)))
 
-(defn- rotate-row [row n grid]
+(defn- rotate-row [[row n] grid]
   (->> (nth grid row)
        (rotate n)
        (assoc grid row)))
 
-(defn- rotate-column [column n grid]
+(defn- rotate-column [[column n] grid]
   (->> (transpose grid)
-       (rotate-row column n)
+       (rotate-row [column n])
        (transpose)))
 
 (defn- lit [grid]
   (apply + (flatten grid)))
 
+(defmulti operation
+  (fn [grid instruction] (:type instruction)))
+
+(defmethod operation :rect [grid instruction]
+  (rect (:value instruction) 1 grid))
+
+(defmethod operation :rotate-row [grid instruction]
+  (rotate-row (:value instruction) grid))
+
+(defmethod operation :rotate-column [grid instruction]
+  (rotate-column (:value instruction) grid))
+
 (defn pixels [grid input]
-  0)
+  (->> input
+       (reduce operation grid)
+       (lit)))
--- day-eight/test/day_eight/core_test.clj
+++ day-eight/test/day_eight/core_test.clj
@@ -3,10 +3,34 @@
             [day-eight.core :refer :all]
             [clojure.string :refer [split]]))
 
+(def rect-regex #"rect (\d+)x(\d+)")
+(def rotate-row-regex #"rotate row y=(\d+) by (\d+)")
+(def rotate-column-regex #"rotate column x=(\d+) by (\d+)")
+
+(defn instruction [type value]
+  {:type type
+   :value (mapv read-string (rest value))})
+
+(defn parse-instruction [input]
+  (let [rect-matches (re-matches rect-regex input)
+        rotate-row-matches (re-matches rotate-row-regex input)
+        rotate-column-matches (re-matches rotate-column-regex input)]
+    (cond
+      rect-matches
+        (instruction :rect rect-matches)
+      rotate-row-matches
+        (instruction :rotate-row rotate-row-matches)
+      rotate-column-matches
+        (instruction :rotate-column rotate-column-matches))))
+
+(defn parse [input]
+  (map parse-instruction input))
+
 (def input
   (-> "resources/input.txt"
       (slurp)
-      (split #"/n")))
+      (split #"\n")
+      (parse)))
 
 (defn grid [[width height] init]
   (->> (repeat width init)
@@ -16,4 +40,4 @@
 
 (deftest pixels-test
   (testing "count of lit pixels"
-    (is (= (pixels (grid [50 6] 0) input) 0))))
+    (is (= (pixels (grid [50 6] 0) input) 116))))

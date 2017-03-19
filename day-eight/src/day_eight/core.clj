2789d2de5e0fabe95a349a23e3dc38f81d2fc1cb
Derek Cuevas <dccuevas2@gmail.com>
2017-03-18 18:57:30 -0700

    added helper functions

--- day-eight/src/day_eight/core.clj
+++ day-eight/src/day_eight/core.clj
@@ -1,7 +1,16 @@
 (ns day-eight.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn- grid [[width height] init]
+  (->> (repeat width init)
+       (vec)
+       (repeat height)
+       (vec)))
+
+(defn- transpose [coll]
+  (apply mapv vector coll))
+
+(defn- rotate [n coll]
+  (let [offset (- (count coll) (mod n (count coll)))]
+    (vec (concat (drop offset coll)
+                 (take offset coll)))))
